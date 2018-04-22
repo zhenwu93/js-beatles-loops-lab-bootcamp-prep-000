18f83fd04e29b9fc47787ce6e215676ce658b96a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-22 21:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   for(let i = 0; i < musicians.length; i++ ) {
     array.push(musicians[i] + " plays " + instruments[i]);
   }
-  return array;
+  return array
 }
 
 function johnLennonFacts(facts) {
